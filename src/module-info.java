4d2100faa04bef63ceaa2f94ccbccf3209f99ca5
CARlos <pruebasi@gmail.com>
2022-03-25 18:33:24 +0100

    comienzo de proyecto
    agregadas variables

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module pruebasgit {
+}
---
+++ src/pruebasgit/EjemploGit.java
@@ -0,0 +1,12 @@
+package pruebasgit;
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+			System.out.println("hola a todos soy carlos");
+			
+			String j = " carlos";
+	}
+
+}
