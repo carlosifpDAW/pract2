02cfeec3848eea13efeea943a39930972a276ac1
CARlos <pruebasi@gmail.com>
2022-03-26 18:47:08 +0100

    introduccion variable int

--- src/pruebasgit/EjemploGit.java
+++ src/pruebasgit/EjemploGit.java
@@ -5,6 +5,7 @@ public class EjemploGit {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 			System.out.println("hola a todos soy carlos");
+			int fe= 32;
 			
 			String j = " des";
 			
